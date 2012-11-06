21cd39b0488b9edcc0a6ee1b6b2d52ea80f34590
Karol Balko <balko.karol@gmail.com>
2012-11-06 14:35:51 +0100

    Implementation of URL Helper

--- OwNetClient/proxy/proxyrequest.h
+++ OwNetClient/proxy/proxyrequest.h
@@ -27,9 +27,13 @@ public:
     ProxyRequest::RequestType requestType();
     const ListOfStringPairs &requestHeaders() { return m_requestHeaders; }
 
-    const QString url() { return m_url; }
+    const QString &url() { return m_url; }
     const QString requestContentType();
-    const QString relativeUrl() { return m_relativeUrl; }
+    const QString &relativeUrl() { return m_relativeUrl; }
+    const QString &action() { return m_action; }
+    const QString &module() { return m_module; }
+    const int &id() { return m_id; }
+    const QMap<QString, QString> &parameters() { return m_parameters; }
 
     int hashCode() { return m_hashCode; }
 
@@ -46,6 +50,10 @@ private:
     QString m_domain;
     QString m_subDomain;
     QString m_url;
+    QString m_module;
+    QString m_action;
+    int m_id;
+    QMap<QString, QString> m_parameters;
 
     QTcpSocket *m_socket;
     static QMap<QString, QString> m_contentTypes;
