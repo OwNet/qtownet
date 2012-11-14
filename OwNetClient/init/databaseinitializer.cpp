50d9254624c758cafcd75af5043cabb38d07373c
Martin Lipták <mliptak@gmail.com>
2012-11-14 10:11:10 +0100

    Add migrations.

--- OwNetClient/init/databaseinitializer.cpp
+++ OwNetClient/init/databaseinitializer.cpp
@@ -1,12 +1,14 @@
 #include "databaseinitializer.h"
 #include "messagehelper.h"
 
+#include "applicationdatastorage.h"
 #include "proxydownloads.h"
 
 #include <QtCore/QVariant>
 #include <QSqlDatabase>
 #include <QSqlError>
 #include <QSqlQuery>
+#include <QTextStream>
 
 DatabaseInitializer::DatabaseInitializer()
 {
@@ -14,26 +16,80 @@ DatabaseInitializer::DatabaseInitializer()
 
 void DatabaseInitializer::init()
 {
-    // init application database
+    openDatabase();
+    runMigrations();
+}
 
+/**
+ * @brief Opens singleton application database.
+ */
+void DatabaseInitializer::openDatabase()
+{
     QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
     db.setDatabaseName("ownet.sqlite");
-
 
     if (!db.open()) {
         MessageHelper::error(QObject::tr("Failed to open database"),
                                 QObject::tr("Unable to open the database: %1.")
                              .arg(db.lastError().text()));
     }
-
-    //User Table init
-    createUserTable();
-
-    // TODO: Close?
 }
 
-void DatabaseInitializer::createUserTable()
+/**
+ * @brief Runs all new migrations from migrations directory.
+ */
+void DatabaseInitializer::runMigrations()
 {
-     QSqlQuery q;
-     q.exec("create table Users (id INTEGER PRIMARY KEY,name TEXT);");
+    // migrations table
+    QSqlQuery query;
+    query.exec("CREATE TABLE IF NOT EXISTS migrations (name TEXT PRIMARY KEY); ");
+
+    // in migrations directory
+    QDir appDataDir = ApplicationDataStorage().appDataDirectory();
+    if (appDataDir.exists("migrations")) {
+        QDir migrationsDir(appDataDir.absolutePath().append("/migrations"));
+
+        QFileInfoList list = migrationsDir.entryInfoList(QDir::Files, QDir::Name);
+
+        // for all migrations
+        for (int i = 0; i < list.size(); ++i) {
+            QSqlQuery query;
+            query.prepare("SELECT name FROM migrations WHERE name = ?; ");
+            query.addBindValue(list.at(i).fileName());
+            query.exec();
+
+            // unless migration has been run
+            if (! query.next())
+            {
+                MessageHelper::debug(QObject::tr("Running migration %1.")
+                                     .arg(list.at(i).fileName()));
+
+                // read file
+                QFile file(list.at(i).absoluteFilePath());
+                if(file.open(QIODevice::ReadOnly)) {
+                    QTextStream in(&file);
+
+                    // execute each line
+                    QSqlQuery query;
+                    while (!in.atEnd()) {
+                        QString line = in.readLine();
+                        query.exec(line);
+                    }
+
+                    file.close();
+                }
+                else {
+                    MessageHelper::debug(QObject::tr("%1: %2.")
+                                         .arg(file.errorString())
+                                         .arg(list.at(i).fileName()));
+                }
+
+                // add to migrations table
+                QSqlQuery query;
+                query.prepare("INSERT INTO migrations (name) VALUES (?); ");
+                query.addBindValue(list.at(i).fileName());
+                query.exec();
+            }
+        }
+    }
 }
