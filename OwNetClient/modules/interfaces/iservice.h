eca279bd995228a188de21041eda3be8672a88ef
Michal Dorner <dorner.michal@gmail.com>
2012-12-09 13:01:07 +0100

    removed unused method

--- OwNetClient/modules/interfaces/iservice.h
+++ OwNetClient/modules/interfaces/iservice.h
@@ -12,10 +12,7 @@ class IService
 {
 public:
     virtual QString name() const = 0;
-
     virtual void init(IRouter* router){}
-
-    virtual IResponse *processRequest(IBus *bus, IRequest *req){ return NULL; }
 };
 
 #endif // ISERVICES_H
