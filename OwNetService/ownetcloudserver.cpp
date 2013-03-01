cee44bdc036b14f4ebd9d56f007427abf5be31e6
Martin Lipták <mliptak@gmail.com>
2013-03-01 15:45:55 +0100

    Send crash reports.

--- OwNetService/ownetcloudserver.cpp
+++ OwNetService/ownetcloudserver.cpp
@@ -1,6 +1,9 @@
 #include "ownetcloudserver.h"
 
 #include <QDebug>
+#include <QUrl>
+#include <QNetworkRequest>
+#include <QNetworkAccessManager>
 
 OwNetCloudServer::OwNetCloudServer(QObject *parent) :
     QObject(parent)
@@ -9,5 +12,14 @@ OwNetCloudServer::OwNetCloudServer(QObject *parent) :
 
 void OwNetCloudServer::sendCrashReport(QString report)
 {
-    qDebug() << report;
+    QNetworkAccessManager *nam = new QNetworkAccessManager(this);
+
+    QString url = "http://localhost:10000";
+    QByteArray postData;
+    postData.append(report);
+
+    QNetworkRequest *request = new QNetworkRequest(QUrl(url));
+    request->setHeader(QNetworkRequest::ContentTypeHeader, "text/plain");
+
+    nam->post(*request, postData);
 }
