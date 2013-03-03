75d1c75b795c2b9985a883c1a5d4e4d20bc6e555
Marek Lani <mareklani@gmail.com>
2013-03-03 19:03:48 +0100

    fixed migration create_activity

--- resources/migrations/20130303-create_activities.sql
+++ resources/migrations/20130303-create_activities.sql
@@ -5,7 +5,7 @@ type INTEGER NOT NULL,
 content  TEXT NOT NULL,
 object_id TEXT NOT NULL,
 group_id INTEGER NOT NULL,
-date_created" TEXT NOT NULL,
-date_updated" TEXT NOT NULL,
-sync_id" TEXT NOT NULL
+date_created TEXT NOT NULL,
+date_updated TEXT NOT NULL,
+sync_id TEXT NOT NULL
 );
