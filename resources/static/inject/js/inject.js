9d4eb65964125705f1cc2151b4c93cbba2a42c01
Martin Konopka <martkono@gmail.com>
2013-03-20 00:03:39 +0100

    remove zero border, not needed

--- resources/static/inject/js/inject.js
+++ resources/static/inject/js/inject.js
@@ -390,7 +390,6 @@
 				img.setAttribute('src', this.imgBaseURI + link.icon)
 				img.setAttribute('alt', link.alt)
 				img.setAttribute('title', link.title)
-				img.setAttribute('border', 0);
 
 				if (link.onclick)
 					a.onclick = self[link.onclick].bind(this)
